e5e957d330f90fdfd7bc1550824b81e8119ace52
opilane <opilane@HP-CZC6297B9H>
2022-12-12 13:36:18 +0200

    Add project files.

---
+++ _12.12_prakt.py
@@ -0,0 +1,79 @@
+#from math import *
+from random import *
+from stringprep import c22_specials, c6_set
+#print("Puu läbimõõdu arvutamine")
+#C=float(input("Anna ümbermõõt"))
+#d=round(C/pi,2)
+#print (f"Puu läbimõõt = {d}")
+## Esimene ülesanne
+
+#print("Ristküliku diagonaali arvutamine: ")
+#input("Kirjuta ristküliku küljed:")
+#N=float(input("Sisesta N: "))
+#M=float(input("Sisesta M: "))
+#d=round(sqrt(N**2+M**2),2)
+#print(f"ristkülikukujulise maatüki diagonaal on {d}")
+##Teine ülesanne
+
+#aeg=float(input("Mitu tundi kulus sõiduks? "))
+#teepikkus=float(input("Mitu kilomeetrit sõitsid? "))
+#kiirus = teepikkus/aeg
+#print("Sinu kiirus oli"+str(kiirus) +"km/h")
+# #kolmas ülesanne
+
+#print("Aritmeetline keskmine ")
+#A1=int(input("Esimene arv: "))
+#A2=int(input("Teine arv: "))
+#A3=int(input("Kolmas arv: "))
+#A4=int(input("Neljas arv: "))
+#A5=int(input("Viies arv "))
+#anwser=(A1+A2+A3+A4+A5)/5
+#print(f"Aritmeetiline keskmine on {anwser}")
+##Neljas ülesanne
+
+#print("Joonista samasugune konn")
+#print(" @..@) " )
+#print(" (----)" )
+#print(" ( \__/ ) ")
+#print(" ^^ "" ^^ ")
+##Viies ülesanne
+
+##6
+#a=randint(0,100)
+#b=randint(0,100)
+#c=randint(0,100)
+#print(f"a={a}\nb={b}\nc={c}")
+#P=a+b+c 
+#print(f"Übermõõt on {P}")
+
+##7
+#P=randint(1,6)
+#summa=(12.9*1.1)/P
+#print=(f"{P}-st, Igaüks maksab {summa} ")
+
+##8
+#print("Kütusekulu arvutamine")
+#l=float(input("Kütuse liitride kogus"))
+#km=float(input("Läbitud kilomeetrid"))
+#kulu=(l/km)*100
+#print(f"KÜTUSEKULU {kulu}")
+
+##9
+#print("Rulluisutajad")
+#m=int(input("Minutid:"))
+#m=m/60
+#tee=m*29.9
+#print(f"Jõuab {tee} km")
+
+#10
+print("Ajateisendus")
+m=int(input("Sisesta aja minutites"))
+h=m//60
+m=m%60
+print(f"{H}:{M}")
+
+      
+
+
+
+
